ded978c9b0e857ba985bb33b457d2ff2c5fb86ca
Rustam <pkheshkhov0707@mail.ru>
2022-12-16 14:48:16 +0300

    первая задача

---
+++ задача 1/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.*/
+
+int Getnumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+           break; 
+        }
+        else
+        {
+            Console.WriteLine("ввели некорректную команду");
+        }
+        
+    }
+    return result;
+}
+
+
+double[,] Getmatix(int m, int n)
+{
+    double[,]array = new double[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(0,11)+rnd.NextDouble();
+;
+;
+;
+        }
+    }
+
+    return array;
+}
+
+void Printmatrix(double[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+     Console.WriteLine();   
+    }
+ 
+}
+
+int m = Getnumber("введите число строк");
+int n = Getnumber("введите число столбцов");
+
+double[,]array = Getmatix(m,n);
+Printmatrix(array);
